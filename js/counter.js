5dc6dde09e10d76ff0bad874f6b62c5dd3d8869d
zznash24 <zznash24@aol.com>
2023-03-23 16:54:28 -0400

    fixed the position of the hamburger menu

--- js/counter.js
+++ js/counter.js
@@ -5,7 +5,7 @@
  * Copyright (c) 2009 Michael Hixson
  * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
 */
-(function($){$.fn.appear=function(f,o){var s=$.extend({one:true},o);return this.each(function(){var t=$(this);t.appeared=false;if(!f){t.trigger('appear',s.data);return;}var w=$(window);var c=function(){if(!t.is(':visible')){t.appeared=false;return;}var a=w.scrollLeft();var b=w.scrollTop();var o=t.offset();var x=o.left;var y=o.top;if(y+t.height()>=b&&y<=b+w.height()&&x+t.width()>=a&&x<=a+w.width()){if(!t.appeared)t.trigger('appear',s.data);}else{t.appeared=false;}};var m=function(){t.appeared=true;if(s.one){w.unbind('scroll',c);var i=$.inArray(c,$.fn.appear.checks);if(i>=0)$.fn.appear.checks.splice(i,1);}f.apply(this,arguments);};if(s.one)t.one('appear',s.data,m);else t.bind('appear',s.data,m);w.scroll(c);$.fn.appear.checks.push(c);(c)();});};$.extend($.fn.appear,{checks:[],timeout:null,checkAll:function(){var l=$.fn.appear.checks.length;if(l>0)while(l--)($.fn.appear.checks[l])();},run:function(){if($.fn.appear.timeout)clearTimeout($.fn.appear.timeout);$.fn.appear.timeout=setTimeout($.fn.appear.checkAll,20);}});$.each(['append','prepend','after','before','attr','removeAttr','addClass','removeClass','toggleClass','remove','css','show','hide'],function(i,n){var u=$.fn[n];if(u){$.fn[n]=function(){var r=u.apply(this,arguments);$.fn.appear.run();return r;}}});})(jQuery);
+(function ($) { $.fn.appear = function (f, o) { var s = $.extend({ one: true }, o); return this.each(function () { var t = $(this); t.appeared = false; if (!f) { t.trigger('appear', s.data); return; } var w = $(window); var c = function () { if (!t.is(':visible')) { t.appeared = false; return; } var a = w.scrollLeft(); var b = w.scrollTop(); var o = t.offset(); var x = o.left; var y = o.top; if (y + t.height() >= b && y <= b + w.height() && x + t.width() >= a && x <= a + w.width()) { if (!t.appeared) t.trigger('appear', s.data); } else { t.appeared = false; } }; var m = function () { t.appeared = true; if (s.one) { w.unbind('scroll', c); var i = $.inArray(c, $.fn.appear.checks); if (i >= 0) $.fn.appear.checks.splice(i, 1); } f.apply(this, arguments); }; if (s.one) t.one('appear', s.data, m); else t.bind('appear', s.data, m); w.scroll(c); $.fn.appear.checks.push(c); (c)(); }); }; $.extend($.fn.appear, { checks: [], timeout: null, checkAll: function () { var l = $.fn.appear.checks.length; if (l > 0) while (l--) ($.fn.appear.checks[l])(); }, run: function () { if ($.fn.appear.timeout) clearTimeout($.fn.appear.timeout); $.fn.appear.timeout = setTimeout($.fn.appear.checkAll, 20); } }); $.each(['append', 'prepend', 'after', 'before', 'attr', 'removeAttr', 'addClass', 'removeClass', 'toggleClass', 'remove', 'css', 'show', 'hide'], function (i, n) { var u = $.fn[n]; if (u) { $.fn[n] = function () { var r = u.apply(this, arguments); $.fn.appear.run(); return r; } } }); })(jQuery);
 
 (function ($) {
 	$.fn.countTo = function (options) {
@@ -14,11 +14,11 @@
 		return $(this).each(function () {
 			// set options for current element
 			var settings = $.extend({}, $.fn.countTo.defaults, {
-				from:            $(this).data('from'),
-				to:              $(this).data('to'),
-				speed:           $(this).data('speed'),
+				from: $(this).data('from'),
+				to: $(this).data('to'),
+				speed: $(this).data('speed'),
 				refreshInterval: $(this).data('refresh-interval'),
-				decimals:        $(this).data('decimals')
+				decimals: $(this).data('decimals')
 			}, options);
 
 			// how many times to update the value, and how much to increment the value on each update
@@ -49,7 +49,7 @@
 
 				render(value);
 
-				if (typeof(settings.onUpdate) == 'function') {
+				if (typeof (settings.onUpdate) == 'function') {
 					settings.onUpdate.call(self, value);
 				}
 
@@ -59,7 +59,7 @@
 					clearInterval(data.interval);
 					value = settings.to;
 
-					if (typeof(settings.onComplete) == 'function') {
+					if (typeof (settings.onComplete) == 'function') {
 						settings.onComplete.call(self, value);
 					}
 				}
